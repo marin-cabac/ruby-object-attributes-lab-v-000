c0dbf163bb3d362a6649ab47451c1166dd373261
Marin Cabac <marin.cabac@gmail.com>
2018-02-20 18:15:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
+
+class Dog
+  def initialize()
+    @name=name
+    @breed=breed
+ end
+  def name(name)
+    @name
+  end
+ def name=(new_name)
+    @name = new_name
+  end
+  def breed(breed)
+    @breed
+  end
+ def breed=(new_breed)
+    @breed = new_breed
+  end
+end
+
+=begin
 class Dog
  def initialize()
 
@@ -7,3 +28,4 @@ class Dog
  attr_accessor :name,:breed
 
 end
+=end
